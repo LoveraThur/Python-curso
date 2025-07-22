3c9cb832e9b0cbb80a789dfaffdfe21f71ef11ae
unknown <arthuralbaralovera@gmail.com>
2025-07-22 20:38:15 -0300

    ex88

--- Mundo 3/desafios/desafio87_Aula17.py
+++ Mundo 3/desafios/desafio87_Aula17.py
@@ -1,12 +1,16 @@
 #Aprimore o desafio 86(matriz), mostrando no final: a) A soma de todos valores pares digitados. b) A soma dos valores da terceira coluna. c) O maior valor da segunda linha
 
-matriz = [[0,0,0], [0,0,0], [0,0,0]]
+matriz = [
+    [0,0,0], 
+    [0,0,0], 
+    [0,0,0]]
 par = 0
 coluna3 = 0
-maior_linha_2 = 0
+
 print('=-'*25)
 print('Criador de Matriz 3x3')
 print('=-'*25)
+
 #criar a matriz
 for linha in range(0,3):
     for coluna in range(0,3):
@@ -14,16 +18,14 @@ for linha in range(0,3):
 #a) numeros pares
         if matriz[linha][coluna] % 2 == 0:
             par = par + matriz[linha][coluna] 
-#b) soma dos valores da 3º coluna *****CONFERIR*****
+#b) soma dos valores da 3º coluna 
         if coluna == 2:
             coluna3 = coluna3 + matriz[linha][coluna]
-#c) maior da segunda            
-        # if linha == 1:
-        #     for num in range(matriz[1]):
-        #         if num > maior_linha_2:
-        #             maior_linha_2 = num
+#c) maior da segunda linha          
 maior_valor = max(matriz[1])
+
 print('=-'*25)
+
 #escrever a matriz de forma correta 
 for linha in range(0,3):
     for coluna in range(0,3):
@@ -33,4 +35,4 @@ for linha in range(0,3):
 print('=-'*25)
 print(f'A soma dos valores pares desta matriz é: {par}')
 print(f'A soma dos valores da 3º coluna é: {coluna3}')
-print(maior_valor)
+print(f'O maior valorr da 2º linha é {maior_valor}')
---
+++ Mundo 3/desafios/desafio88_Aula17.py
@@ -0,0 +1,29 @@
+#faça um programa que ajuda um jogador da MEGA SENA a criar palpites. O programa vai perguntar quantos jogos serão gerados e vai sortear 6 numeros para cada jogo, cadastrando tudo em uma lista composta
+
+from random import randint
+from time import sleep
+
+print('='*30)
+print('GERADOR DE PALPITE MEGA SENA')
+print('='*30)
+mega_senas = int(input('Quantos jogos você quer sortear? '))
+lista = []
+jogos = []
+
+print('=-'*3, end=' ')
+print(f'SORTEANDO {mega_senas} JOGOS', end=' ')
+print('=-'*3)
+for jogo in range(mega_senas):
+    for a in range(6):
+        numero = (randint(0,61))
+        if numero in lista:
+            numero = (randint(0,61))
+        lista.append(numero)
+
+        jogos = lista[:]
+    lista.clear()
+    print(f'Jogo {jogo+1}: {sorted(jogos)}')
+    sleep(1.5)
+print('=-'*4+'> ', end= '')
+print('BOM JOGO!', end=' ')
+print(' <'+'=-'*4,)
