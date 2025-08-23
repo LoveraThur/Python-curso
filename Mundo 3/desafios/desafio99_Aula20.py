4a93cb2a35ba7ef9a5102abc2ac95496d76450af
unknown <arthuralbaralovera@gmail.com>
2025-08-22 23:35:18 -0300

    ex99

---
+++ Mundo 3/desafios/desafio99_Aula20.py
@@ -0,0 +1,12 @@
+#faça um programa que tenha uma função chamada maior(que receba varios parâmetros com valores inteiros. Seu programa tem que analisar todos os valores e dizer qual deles é o maior.
+from time import sleep
+def maior(numeros):
+    print('analisando valores passados...')
+    sleep(1)
+    print(f'Foram informados {len(numeros)}Os valores informados foram: ', end=' ')
+    for num in numeros:
+        print(num, end=' ')
+    print()
+    maior = max(numeros)
+    print(f'O maior número informado é: {maior}')
+maior([1, 6, 8, 20, 5, 60 ])
