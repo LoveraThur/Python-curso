e599946fd86906a4b97e40fdd3e8869eeeb91521
Arthur Lovera <arthuralbaralovera@gmail.com>
2025-11-04 08:22:59 -0300

    atualização

--- Mundo 3/desafios/desafio98_Aula20.py
+++ Mundo 3/desafios/desafio98_Aula20.py
@@ -1,4 +1,4 @@
-#Faça um programa que tenha uma função chamada contador(), que receba trÊs parâmetros: inicio, fim e passo e reralize a contagem. Seu programa tem que realizar três contagens através da função criada. a) de 1 até 10, de 1 em 1. b) de 10 até 0, de 2 em 2. c) uma contagem personalizada
+#Faça um programa que tenha uma função chamada contador(), que receba trÊs parâmetros: inicio, fim e passo e reralize a contagem. Seu programa tem que realizar três contagens através da função criada. a) de 1 até 10, de 1 em 1. b) de 10 até 0, de 2 em 2. c) uma contagem personalizad
 
 def contador(inicio, fim, pula):
     if pula == 0:
